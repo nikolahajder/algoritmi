204268a05b52de3045a2b256b1c7f0ef30effa39
Nikola <nikola.hajder@comdata.com>
2022-10-12 12:11:55 +0200

    bolded substring

--- src/app/algorithms/substring/substring.component.ts
+++ src/app/algorithms/substring/substring.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { first } from 'rxjs';
 import { SolutionComponent } from 'src/app/solution.component';
 import { IAlgorithm } from '../algorithms';
 import { AlgorithmsService } from '../algorithms.service';
@@ -59,36 +58,29 @@ export class SubstringComponent implements OnInit, SolutionComponent {
         })
     }
 
-    printLCSubStr(X,Y)
-    {
-        // Create a table to store lengths of longest common
-        // suffixes of substrings.   Note that LCSuff[i][j]
-        // contains length of longest common suffix of X[0..i-1]
-        // and Y[0..j-1]. The first row and first column entries
-        // have no logical meaning, they are used only for
-        // simplicity of program
+    printLCSubStr(X, Y) {
         let m = X.length;
         let n = Y.length;
-        let LCSuff = new Array(m+1);
-         
+        let LCSuff = new Array(m + 1);
+
         // To store length of the longest common substring
         let len = 0;
-  
+
         // To store the index of the cell which contains the
         // maximum value. This cell's index helps in building
         // up the longest common substring from right to left.
         let row = 0, col = 0;
-  
+
         /* Following steps build LCSuff[m+1][n+1] in bottom
            up fashion. */
         for (let i = 0; i <= m; i++) {
-            LCSuff[i] = Array(n+1);
+            LCSuff[i] = Array(n + 1);
             for (let j = 0; j <= n; j++) {
-                LCSuff[i][j]=0;   
+                LCSuff[i][j] = 0;
                 if (i == 0 || j == 0)
                     LCSuff[i][j] = 0;
-  
-                else if (X[i-1] == Y[j-1]) {
+
+                else if (X[i - 1] == Y[j - 1]) {
                     LCSuff[i][j] = LCSuff[i - 1][j - 1] + 1;
                     if (len < LCSuff[i][j]) {
                         len = LCSuff[i][j];
@@ -100,31 +92,32 @@ export class SubstringComponent implements OnInit, SolutionComponent {
                     LCSuff[i][j] = 0;
             }
         }
-  
+
         // if true, then no common substring exists
         if (len == 0) {
-            this.substring ="No Common Substring";
+            this.substring = "No Common Substring";
             this.displayAnswer = true;
             return;
         }
-  
+
         // allocate space for the longest common substring
         let resultStr = "";
-  
+
         // traverse up diagonally form the (row, col) cell
         // until LCSuff[row][col] != 0
         while (LCSuff[row][col] != 0) {
-            resultStr = X[row-1] + resultStr; // or Y[col-1]
+            resultStr = X[row - 1] + resultStr; // or Y[col-1]
             --len;
-  
+
             // move diagonally up to previous cell
             row--;
             col--;
         }
-  
+
         this.substring = resultStr;
         this.displayAnswer = true;
     }
+
 
     onChange(newValue) {
         this.displayAnswer = false;
